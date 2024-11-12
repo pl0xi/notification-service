ccd40baa8998620146042f2c3a6112e22f587538
Benjamin Sommervoll <puixdk@gmail.com>
2024-11-12 01:43:16 +0100

    WIP Duplicate check for shopify events

--- src/db/queries/events/get.rs
+++ src/db/queries/events/get.rs
@@ -1,1 +1,16 @@
+use deadpool_postgres::Client;
+use thiserror::Error;
+use tokio_postgres::Row;
 
+#[derive(Debug, Error)]
+pub enum GetEventError {
+    #[error("Failed to get event")]
+    FailedToGetEvent,
+}
+
+pub async fn get_event(client: &Client, event_id: &str) -> Result<Row, GetEventError> {
+    let query = "SELECT * FROM events WHERE id = $1";
+    let row = client.query_one(query, &[&event_id]).await.map_err(|_| GetEventError::FailedToGetEvent)?;
+
+    Ok(row)
+}

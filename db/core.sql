e35650dca5d4950f6e82ee472adfca5878100f27
Benjamin Sommervoll <puixdk@gmail.com>
2024-11-14 16:25:07 +0100

    Template DB schema

--- db/core.sql
+++ db/core.sql
@@ -1,3 +1,24 @@
 CREATE TABLE IF NOT EXISTS events (
     event_id VARCHAR(255) PRIMARY KEY
 );
+
+CREATE TABLE IF NOT EXISTS email_templates (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS email_events_types (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO email_events_types (name) VALUES ('order_created');
+
+CREATE TABLE IF NOT EXISTS email_events (
+    id SERIAL PRIMARY KEY,
+    event_type INTEGER NOT NULL,
+    template_id INTEGER NOT NULL,
+    FOREIGN KEY (event_type) REFERENCES email_events_types(id),
+    FOREIGN KEY (template_id) REFERENCES email_templates(id)
+);
---
+++ src/services/db/queries/email_template/mod.rs
@@ -0,0 +1,3 @@
+pub mod get_by_name;
+
+pub use get_by_name::get_email_template_by_name;
